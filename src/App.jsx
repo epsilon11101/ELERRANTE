aa4d978847fae959b0aa9e22d5b13dc243d7ed48
Aaron MV <epsilon11101@gmail.com>
2023-03-08 08:27:20 -0600

    added new section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Header from "./Components/Layout/Header/Header";
 import Banner from "./Components/Layout/Main/Banner/Banner";
 import Form from "./Components/Layout/Main/Form/Form";
+import Adventure from "./Components/Layout/Main/Adventure/Adventure";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Banner />
       <Form />
+      <Adventure />
     </Fragment>
   );
 }
